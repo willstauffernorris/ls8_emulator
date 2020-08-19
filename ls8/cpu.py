78aeee5e0f0a291c946cdf2231b8bb8a742170b9
willsn <willsn@gmail.com>
2020-08-19 08:48:47 -0700

    multiply works

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -35,6 +35,15 @@ class CPU:
         #Bonus: check to make sure the user has put a command line argument where you expect, and print an error and exit if they didn't.
         program = sys.argv[1]
 
+        with open(program) as f:
+            for line in f:
+                comment_split = line.split("#")
+                n = comment_split[0].strip()
+                x = int(n, 2)
+                self.ram[address] = x
+                address += 1
+                # print(x)
+
         # For now, we've just hardcoded a program:
 
         # program = [
@@ -47,10 +56,10 @@ class CPU:
         #     0b00000001, # HLT
         # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-            print(instruction)
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        #     print(instruction)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -58,6 +67,9 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+
+        elif op == "MUL":
+            self.general_purpose_register[reg_a] *= self.general_purpose_register[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -87,13 +99,17 @@ class CPU:
         #starts at 0
         self.PC = 0
 
-
-
+        #print(f'RAM: {self.ram}')
+        #print(f'General Purpose Register: {self.general_purpose_register}')
 
 
         # dpending on the value of the opcode
         # perform the actions needed for the instruction
-        while self.PC < 8:
+        while self.PC < 256:
+            #print(f'PC: {self.PC}')
+            # print(f'RAM: {self.ram}')
+            # print(f'General Purpose Register: {self.general_purpose_register}')
+
 
             operand_a = self.ram_read(self.PC+1)
             operand_b = self.ram_read(self.PC+2)
@@ -103,21 +119,29 @@ class CPU:
 
             #if HLT:
             if IR == 0b00000001:
-                # print("HLT!!!")
+                print("HLT!!!")
                 exit()
 
             #elif LDI
             elif IR == 0b10000010:
                 #Set the value of a register to an integer.
-                # print("LDI")
+                print("LDI")
                 #IR = int(IR)
                 self.general_purpose_register[operand_a] = operand_b
+                self.PC += 2
+                #print(self.general_purpose_register)
 
 
             #elif PRN`
             elif IR == 0b01000111:
-                # print("PRN")
+                print("PRN")
                 print(self.general_purpose_register[operand_a])
+                self.PC += 1
+            
+            elif IR == 0b10100010:
+                print("MUL")
+                self.alu(op="MUL", reg_a=operand_a, reg_b=operand_b)
+                self.PC += 2
             
 
             ## after running the instructions, 
